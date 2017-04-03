f9567e600b2aa273e5d4a384edcf3dfe0fd0062d
sudheeshcm <sudheeshcmohan@gmail.com>
2017-04-03 18:58:25 +0530

    Added Hero service - Promise based heros list fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
-console.log('HEROES: ', HEROES);
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -32,14 +18,24 @@ console.log('HEROES: ', HEROES);
     </ul>
     <hero-detail [hero]='selectedHero'></hero-detail>
   </div>`,
+  providers: [HeroService],
 })
 
-export class AppComponent  {
+export class AppComponent implements OnInit {
   title = 'Angular 2 Heros';
+  heroes: Hero[];  
   selectedHero: Hero;
-  heroes = HEROES;
+
+  constructor(private heroService: HeroService) {}
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  };
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   };
-
 }
